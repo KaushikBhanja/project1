d51f65de6a533108f9e6afa65ab3ed35fcf5a87e
Kaushik Bhanja <Kaushik.Bhanja@unh.edu>
2022-08-10 19:58:52 -0400

    6th line was missing in the previous commit

--- project/KdV.h
+++ project/KdV.h
@@ -3,5 +3,6 @@
 
 void rk4(double x[], double xnew[], double dt, double dx);
 void kdv(double x[], double xnew[], double dt, double dx);
+void kdvExact(double x[], double t, double v, double x0);
 
 #endif
