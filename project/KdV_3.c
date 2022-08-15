162bc26992cd40a2f8890b7ba0bb903c4b97085c
Kaushik Bhanja <Kaushik.Bhanja@unh.edu>
2022-08-15 13:15:10 -0400

    modified and final version of this code after adding the parallelisation

--- project/KdV_3.c
+++ project/KdV_3.c
@@ -2,6 +2,9 @@
 #include<math.h>
 #include<stdlib.h>
 
+#define DX 0.1
+#define L  8
+#define DT 1e-4
 #define N  160
 
 void kdvExact(double x[], double t, double v, double x0) {
@@ -9,6 +12,7 @@ void kdvExact(double x[], double t, double v, double x0) {
 	int i;
 	double denom;
 
+	#pragma omp parallel for
 	for(i = 0; i < N; i++) {
       denom = cosh(0.5 * sqrt(v) * (x[i] - v * t - x0));
 	  x[i] = v / (2. * denom * denom);
