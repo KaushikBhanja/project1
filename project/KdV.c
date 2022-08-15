c8fe0a5395502417e00d0cb19958f1a60f2e8158
Kaushik Bhanja <Kaushik.Bhanja@unh.edu>
2022-08-15 01:54:50 -0400

    modified version after adding "Wtime" header file, time command and parallelisation

--- project/KdV.c
+++ project/KdV.c
@@ -1,3 +1,4 @@
+#include "Wtime.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -18,34 +19,46 @@ int main(void) {
 	double *x    = (double *) malloc(N * sizeof(double));
 	double *xnew = (double *) malloc(N * sizeof(double));
 
+	double t1 = Wtime();
+	#pragma omp parallel for
 	for(i = 0; i < N; i++) {
 	    x[i] = -L + i * DX;
 		xnew[i] = -L + i * DX;
 	}
+	double t2 = Wtime();
 
 	/* 
 	 * Initial Conditions
 	 */
 	kdvExact(x, 0., 16., 4.);
-	kdvExact(xnew, 0., 4., -4.);
+	kdvExact(xnew, 0, 4., -4.);
 
+	double t3 = Wtime();
 	for(i = 0; i < N; i++) {
 		x[i] = x[i] + xnew[i];
 	}
+	double t4 = Wtime();
 
 	t = 0.;
 	j = 0;
+
 	while(t < 2) {
 
+		double t5 = Wtime();
 		if((j % 100) == 0) {
+			
 	        for(i = 0; i < N; i++) {
 				plot1 = -L + i*DX;
 				plot2 = x[i];
 				printf("%e \n", plot2);
-		        //printf("%e %e\n", -L + i * DX, x[i]);
+				
 	        }
+			
 			printf("\n\n");
+			printf("1st time taken %f\n", t2-t1);
+			printf("2nd time taken %f\n", t4-t3);
 		}
+		double t6 = Wtime();
 
 		rk4(x, xnew, DT, DX);
 
